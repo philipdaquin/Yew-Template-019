18aa6fda31ffccf58970076f211fae269a9525ff
philipdaquin <85416532+philipdaquin@users.noreply.github.com>
2022-02-23 12:19:02 +0800

    Optmised

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ pub mod components;
 mod hooks;
 pub mod router;
 
-
+use wasm_bindgen::prelude::*;
 use crate::app::App;
 
-use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 pub fn main() -> Result<(), JsValue> {
+    #[cfg(debug_assertions)]
     wasm_logger::init(wasm_logger::Config::default());
     yew::start_app::<App>();
     Ok(())
